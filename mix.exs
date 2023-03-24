818518e497f9914c974e84f2df18625beb6a622b
Chris Bailey <me@cbailey.co.uk>
2023-03-24 13:14:40 +0000

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Endo.MixProject do
   def project do
     [
       app: :endo,
-      version: "0.1.12",
+      version: "0.1.13",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
