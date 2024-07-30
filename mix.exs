234277f92914ef1e8285222df7a7e31f2c22b8c8
Bill <kontoswilliam@gmail.com>
2024-07-30 12:37:38 -0400

    list_tables/2 configurable timeout

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Endo.MixProject do
   def project do
     [
       app: :endo,
-      version: "0.1.20",
+      version: "0.1.21",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
